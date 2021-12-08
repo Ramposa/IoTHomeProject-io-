b358217055333b3c44d60c4902ec84d86241bb61
Daniel Tang <33831500+Ramposa@users.noreply.github.com>
2021-12-08 07:35:47 +0000

    Update IoTProject.ino

--- IoTProgram/Main/IoTProject.ino
+++ IoTProgram/Main/IoTProject.ino
@@ -1,27 +1,25 @@
 #include <WiFiNINA.h> // Libery to connect the WiFi
 #include <ThingSpeak.h> // 
 #include "dht.h"
+
 #define Atomizer 0 // A0
 #define AirPIN 1 // A1
 #define MQ2pin 2 // A2
-#define motorPin 4
-//int motorPin = 4;
+
 float sensorValue;  //variable to store sensor value
 float atomizerValue;
 float airValue;
-float motorValue;
 const int dht_apin = A3; // Analog Pin sensor is connected to A3
 char ssid[] = "IoTTestNetwork"; // Network name
-const char password [] = "7HzOTHxq"; // PWD
-const unsigned long channel_id = 1573881; // CH ID
-const char write_api_key[] = "CTXB3D7BB16SI71Y";// API key LOOK INNTO API KEYS TAB
+const char password [] = ""; // PWD
+const unsigned long channel_id = ; // CH ID
+const char write_api_key[] = "";// API key LOOK INNTO API KEYS TAB
 
 dht DHT; // Initalize dht to DHT
 WiFiClient client; // Connect to to a specified internet IP address
 
 void setup() {
   // Setting up connection
-  pinMode(motorPin, OUTPUT);
   Serial.begin(9600);
   delay(100);
   Serial.print("Connecting to " + String(ssid));
@@ -34,14 +32,14 @@ void setup() {
   Serial.println();
   Serial.println("Connected!");
   ThingSpeak.begin(client);
+  
   }
 
 void loop() {
 
   airValue = analogRead(AirPIN); // read analog input pin 1
   sensorValue = analogRead(MQ2pin); // read analog input pin 2
-  motorValue = analogRead(motorPin); // read Digital pww pin 2
-  DHT.read22(dht_apin); // Read from pin3
+  DHT.read22(dht_apin);
   
   Serial.println("Posting Humdity " + String(DHT.humidity, 2) + "% to ThingSpeak");
   ThingSpeak.setField(1, String(DHT.humidity, 2));
@@ -54,38 +52,32 @@ void loop() {
 
 // GROVE START IF STATEMENT
   if(DHT.humidity>=50){ //if the humidity % is equal or above the setpoint 51% the atomizer turn ON (HIGH)
-      digitalWrite(Atomizer,LOW);
-      digitalWrite(motorPin, HIGH);
-      Serial.print("ATOMIZER OFF \n MOTOR ON \n");
-      Serial.println("Posting Atomizer OFF" + String(atomizerValue, 0) + String(motorValue, 1) + " to ThingSpeak");
-      ThingSpeak.setField(4, String(atomizerValue= 0));
-      ThingSpeak.setField(6, String(motorValue= 1));
+    digitalWrite(Atomizer,LOW);
+    Serial.print("OFF \n");
+    Serial.println("Posting Atomizer OFF" + String(atomizerValue, 0) + " to ThingSpeak");
+    ThingSpeak.setField(4, String(atomizerValue= 0));
     }
 
     else if(DHT.humidity<=30){ 
-      digitalWrite(Atomizer,HIGH);
-      digitalWrite(motorPin, LOW);
-      Serial.print("ATOMIZER ON \n MOTOR OFF \n");;
-      Serial.println("Posting Atomizer ON" + String(atomizerValue, 1) + String(motorValue, 0) +  " to ThingSpeak");
-      ThingSpeak.setField(4, String(atomizerValue = 1));
-      ThingSpeak.setField(6, String(motorValue= 0));
+    digitalWrite(Atomizer,HIGH);
+    Serial.print("ON \n");
+    Serial.println("Posting Atomizer ON" + String(atomizerValue, 1) + " to ThingSpeak");
+    ThingSpeak.setField(4, String(atomizerValue = 1));
     }
 
     else if(DHT.humidity){
       digitalWrite(Atomizer,LOW);
-      digitalWrite(motorPin, LOW);
-      Serial.print("ATOMIZER OFF \n MOTOR OFF \n");
-      Serial.println("Posting Atomizer OFF " + String(atomizerValue, 0) + String(motorValue, 0) +  " to ThingSpeak");
+      Serial.print("OFF \n");
+      Serial.println("Posting Atomizer OFF " + String(atomizerValue, 0) + " to ThingSpeak");
       ThingSpeak.setField(4, String(atomizerValue = 0));
-      ThingSpeak.setField(6, String(motorValue= 0));
     }
 // GROVE END
 
   Serial.println("Posting " + String(airValue, 2) + " to ThingSpeak");
   ThingSpeak.setField(5, String(airValue, 2));
   
-  //ThingSpeak.writeFields(channel_id, write_api_key);
+  ThingSpeak.writeFields(channel_id, write_api_key);
   Serial.println();
-  //delay(300000); // 5min delay
-  delay(15000); // 15s delay Testing purposes
+  delay(300000); // 5min delay
+  //delay(15000); // 15s delay Testing purposes
 }
